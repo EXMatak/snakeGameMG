d1219637696f5d0b5b64f68016ef31d1dca5390a
EXMatak <matej@netsecure.cz>
2023-12-21 09:40:16 +0100

    reformated, added better body algorithm

--- snakeGameMG.py
+++ snakeGameMG.py
@@ -5,7 +5,7 @@ import turtle
 import chime
 
 colors = ["blue", "red", "green", "pink", "yellow", "dark slate blue", "lavender"]  # pole na nahodne vybirani barev
-score = 0
+score = 19
 highScore = 0
 
 # setup okna
@@ -40,11 +40,6 @@ highScoreDisplay.hideturtle()
 highScoreDisplay.penup()
 highScoreDisplay.goto(0, -260)
 highScoreDisplay.write(f"High Score: {highScore}", align="center", font=("calibri", 24, "bold"))
-
-# zapamatuj si kolik segmentu mas
-segments = []
-coordinates = []
-
 
 # endregion
 
@@ -91,25 +86,26 @@ def left():
 
 
 def move():  # pohyb dopredu
+    previousPosition = snake.position()
+    for member in body:
+        currentPosition = member.position()
+        member.setposition(previousPosition)
+        previousPosition = currentPosition
     snake.forward(20)
-
-
-def clearSegments():  # derenderovani segmentu po smrti
-    for segment in segments:
-        segment.clear()
-        segment.reset()
-    segments.clear()
-
 
 def roundToTwenty(number):  # pouzite na gridy po dvaceti
     return ((int(number) + 19) // 20) * 20
 
+def clearSnakeBody():
+    global body
+    for member in body:
+        member.goto(1000, 1000)
+    body.clear()
 
 def death():  # proste funkce na smrt
     global score
     global highScore
-    clearSegments()
-    coordinates.clear()
+    global body
     snake.goto(0, 0)
     snake.direction = "stop"
     pen.clear()
@@ -122,6 +118,7 @@ def death():  # proste funkce na smrt
         updateHighScore()
         saveHighScore()
     score = 0
+    clearSnakeBody()
     time.sleep(1)
 
 
@@ -133,14 +130,19 @@ def writeScore():  # zapisovani score
 
 
 def testUmrti():  # zkousi jak smrt na borderu tak smrt na vlastni ocas
-    global score
-    if [roundToTwenty(snake.xcor()), roundToTwenty(snake.ycor())] in coordinates[:-1]:
-        death()
-        print("umrel sam na sebe")
+    #global score
+    #if [roundToTwenty(snake.xcor()), roundToTwenty(snake.ycor())] in coordinates[:-1]:
+    #    death()
+    #    print("umrel sam na sebe")
 
     if snake.xcor() > 290 or snake.xcor() < -290 or snake.ycor() > 290 or snake.ycor() < -290:
         death()
         print("umrel na hranice")
+
+    for member in body:
+        if snake.distance(member) < 15:
+            death()
+            print("umrel sam na sebe")
     writeScore()
 
 
@@ -149,61 +151,51 @@ def testJidlo():
     Funkce pro testování, zda hráč snědl jídlo a provedení akcí v případě, že hráč jídlo snědl.
     """
     # jinej komentar pro zkouseni jak se commentuje ve vscodu :D
-    global segment
     global score
     if snake.distance(food) < 20:
         score += 1
         barvaHada = random.choice(colors)  # ac se muze jevit, ze toto ve hre zpusobuje bug, ze se nemeni barva celeho hada, neni to tak
         snake.color(barvaHada)  # tato implemetace simuluje jak se jablko hadovi dostava "do bricha"
-        segment.color(barvaHada)
         chime.success()
         jidloX = random.randint(-13, 13) * 20
         jidloY = random.randint(-13, 13) * 20
         food.goto(jidloX, jidloY)
+        teloHada()
         writeScore()
 
+def teloHada():
+    global body
+    member = turtle.Turtle()
+    member.penup()
+    member.shape("square")
+    member.color("white")
+    member.setpos(snake.position())
+    body.insert(0, member)
 
+def sleep():
+    global score
+    if score < 20:
+        return time.sleep(0.2 - (0.01 * score) + 0.02)
+    else:
+        return time.sleep(0.03)
+
+def listenForKeyInputs():
+    window.listen()  # poslouchani keypressu
+    window.onkeypress(up, "Up")
+    window.onkeypress(right, "Right")
+    window.onkeypress(down, "Down")
+    window.onkeypress(left, "Left")
 # endregion
 
 # region MAIN LOOP A GAME LOOP
-window.listen()  # poslouchani keypressu
-window.onkeypress(up, "Up")
-window.onkeypress(right, "Right")
-window.onkeypress(down, "Down")
-window.onkeypress(left, "Left")
+
 loadHighScore()
+body = []
+listenForKeyInputs()
 while True:
     testUmrti()
-
-    # udelej novej segment a pridej ho k listu
-    segment = turtle.Turtle()
-    segment.shape("square")
-    segment.color("white")
-    segment.penup()
-    segments.append(segment)
-    segment.goto(snake.xcor(), snake.ycor())
-    coordinates.append([roundToTwenty(snake.xcor()), roundToTwenty(snake.ycor())])
-
-    if len(segments) > score:
-        # maz posledni segment, pokud je delsi nez score (cim vetsi score, tim delsi had)
-        segments[0].clear()
-        segments[0].reset()
-        segments.pop(0)
-        coordinates.pop(0)
-
-    # region DEBUG TOOLS
-    # if [int(snake.xcor()), int(snake.ycor())] in poleSouradnic[:-1]:
-    #     print("Aktualni souradnice je v poli")
-    # else:
-    #     print("Aktualni souradnice neni v poli")
-
-    # print(f"X:{roundToTwenty(snake.xcor())} ; Y:{roundToTwenty(snake.ycor())}")
-    # print(coordinates)
-    # print(segments)
-    # endregion
-    move()
-    if score < 20:  # podle skore snizuje tickrate, hra je tim "rychlejsi"
-        time.sleep(0.2 - (0.01 * score))
-    window.update()
     testJidlo()
+    move()
+    window.update()
+    sleep()
 # endregion
